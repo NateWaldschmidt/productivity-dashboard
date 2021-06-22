c1c877043911c4d3989484489c2fa3a4b00e2d15
Nathaniel-Waldschmidt <77284592+Nathaniel-Waldschmidt@users.noreply.github.com>
2021-06-22 15:37:16 -0500

    Added scope around the const being used because it is never used outside of that scope.

--- add-general-item.js
+++ add-general-item.js
@@ -11,19 +11,20 @@ for (let generalList of document.querySelectorAll(".card-container.general-list"
     generalList.querySelector(".add-button").addEventListener("click", function() { addGeneralItem(); });
 }
 
-// Selects the add button inside the template for dynamically added lists.
-const TEMPLATE_ADD_BUTTON = document.getElementById("template-list-general").content.querySelector(".add-button");
-
 // Ensures not to overwrite other onclick events.
-if (TEMPLATE_ADD_BUTTON.getAttribute(onclick) != null) {
-    TEMPLATE_ADD_BUTTON.setAttribute(
-        "onclick",
-        TEMPLATE_ADD_BUTTON.getAttribute(onclick) + "addGeneralItem(this)"
-    );
-} else {
-    TEMPLATE_ADD_BUTTON.setAttribute(
-        "onclick",
-        "addGeneralItem(this)"
-    );
+{
+    // Selects the add button inside the template for dynamically added lists.
+    const TEMPLATE_ADD_BUTTON = document.getElementById("template-list-general").content.querySelector(".add-button");
+    if (TEMPLATE_ADD_BUTTON.getAttribute(onclick) != null) {
+        TEMPLATE_ADD_BUTTON.setAttribute(
+            "onclick",
+            TEMPLATE_ADD_BUTTON.getAttribute(onclick) + "addGeneralItem(this)"
+        );
+    } else {
+        TEMPLATE_ADD_BUTTON.setAttribute(
+            "onclick",
+            "addGeneralItem(this)"
+        );
+    }
 }
 
