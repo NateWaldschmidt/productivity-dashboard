9d2a60a701d4e2b7d5a6b8ab56416a14fc2940fd
Nathaniel-Waldschmidt <77284592+Nathaniel-Waldschmidt@users.noreply.github.com>
2021-06-22 15:15:58 -0500

    Changed the addGeneralItem function to work in a far more dynamic way.  It also has a lot of performance improvements because of this particular change.

--- add-general-item.js
+++ add-general-item.js
@@ -1,10 +1,15 @@
-function addGeneralItem(card) {
-    const TEMPLATE = document.getElementById("template-li-general");
-    const DUPLICATE = TEMPLATE.content.cloneNode(true);
-    card.querySelector("ul").appendChild(DUPLICATE);
+function addGeneralItem(button) {
+    // Gets the <template> and then makes the duplicate.
+    const DUPLICATE = document.getElementById("template-li-general").content.cloneNode(true);
+
+    // Appends a new <li> to the <ul>.
+    button.closest(".card-container").querySelector("ul").append(DUPLICATE);
 }
 
-// Adds the function call to all of the add buttons.
+// Adds the handler to all of the add buttons that already exist.
 for (let generalList of document.querySelectorAll(".card-container.general-list")) {
-    generalList.querySelector(".add-button").addEventListener("click", function() { addGeneralItem(generalList); });
+    generalList.querySelector(".add-button").addEventListener("click", function() { addGeneralItem(); });
 }
+
+// Selects the add button inside the template for dynamically added lists.
+document.getElementById("template-list-general").content.querySelector(".add-button").setAttribute("onclick", "addGeneralItem(this)");
