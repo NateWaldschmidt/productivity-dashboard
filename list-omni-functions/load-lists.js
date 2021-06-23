0ed381a2e0a1c5d2fe571b8e91f2225a205281cb
Nathaniel-Waldschmidt <77284592+Nathaniel-Waldschmidt@users.noreply.github.com>
2021-06-23 11:25:51 -0500

    Added functionality to load data in from the localStorage.

---
+++ list-omni-functions/load-lists.js
@@ -0,0 +1,26 @@
+function loadLists() {
+    for (let listData of JSON.parse(window.localStorage.lists)) {
+        const DUPLICATE = document.getElementById("template-list-general").content.cloneNode(true);
+
+        DUPLICATE.querySelector(".card-container").setAttribute("data-id", listData.id);
+        DUPLICATE.querySelector(".list-title").innerText = listData.title;
+
+        for (let listItem of listData.items) {
+            const DUP_ITEM = document.getElementById("template-li-general").content.cloneNode(true);
+            DUP_ITEM.querySelector(".li-label").innerText = listItem.itemName;
+            DUP_ITEM.querySelector(".li-checkbox").checked      = listItem.checked;
+            DUPLICATE.querySelector("ul").insertBefore(
+                DUP_ITEM,
+                DUPLICATE.querySelector("ul").children[0]
+            );
+        }
+    
+        // Inserts the duplicate before all of the other cards.
+        document.querySelector("main").insertBefore(
+            DUPLICATE,
+            document.querySelector("main").children[0]
+        );
+    }
+}
+
+loadLists();
